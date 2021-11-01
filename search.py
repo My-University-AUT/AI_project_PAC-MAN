2b8f368c7e5d57a5e505d344f41ada4160abc18c
AliNowrouzi <alinowrouzii@gmail.com>
2021-11-01 23:40:21 +0330

    nothing

--- search.py
+++ search.py
@@ -147,7 +147,7 @@ def custom_search(problem, heuristic=nullHeuristic, heapType='min'):
                 cost_from_curr_node_to_this_node = successor[2]
 
                 h_value = heuristic(node, problem)
-
+                
                 node_g_value = cost_from_curr_node_to_this_node + current_node_g_value
 
                 # node_g_value is the real backward cost
@@ -157,7 +157,7 @@ def custom_search(problem, heuristic=nullHeuristic, heapType='min'):
 
                 # if priorityQueue was updated sucessfully, then update that path(storage) and update node g_value
                 if updated:
-                    storage[node] = [curr_node, direction]
+                    storage[node] = [curr_node, direction, h_value]
                     g_vals[node] = node_g_value
 
     goal_path = []
@@ -173,6 +173,9 @@ def custom_search(problem, heuristic=nullHeuristic, heapType='min'):
         # update current node
         curr_node = node[0]
 
+        f = open("heuristic_vals.txt", "a")
+        f.write("node: "+str(node[0])+"  heuristic value: "+ str(node[2])+ "\n")
+        f.close()
         # reach the start state
         if(node[0] == start_state):
             break
@@ -243,7 +246,7 @@ def custom_bfs(problem, startState, goalState):
         # reach the start state
         if(node[0] == start_state):
             break
-    print('steps length: ', len(goal_path))
+
 
     # reverse the path by slicing
     return goal_path[::-1]
